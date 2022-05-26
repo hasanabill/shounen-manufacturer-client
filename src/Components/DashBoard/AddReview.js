c81a6bbdf4fdd8e938f637b3a8bcf693bbbbd321
hasanabill <hasanabilna@gmail.com>
2022-05-26 09:30:38 +0600

    sending review to db

--- src/Components/DashBoard/AddReview.js
+++ src/Components/DashBoard/AddReview.js
@@ -1,9 +1,63 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from './../../firebase.init';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
 
 const AddReview = () => {
+    const [user] = useAuthState(auth)
+    const { register, formState: { errors }, handleSubmit, reset } = useForm();
+
+    const onSubmit = data => {
+        const review = {
+            username: data.username,
+            img: user?.photoURL,
+            review: data.review
+        }
+        fetch('http://localhost:5000/review', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(review)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged) {
+                    reset()
+                    toast.success('Review Added')
+                }
+            })
+    }
     return (
         <div>
-            Add Review
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div class="form-control w-full max-w-sm">
+                    <label class="label">
+                        <span class="label-text">Your Name</span>
+                    </label>
+                    <input
+                        {...register('username')}
+                        type="text" value={user?.displayName} readOnly class="input input-bordered w-full max-w-sm" />
+                </div>
+                <div class="form-control">
+                    <label class="label">
+                        <span class="label-text">Your Review</span>
+                    </label>
+                    <textarea
+                        {...register('review', {
+                            required: {
+                                value: true,
+                                message: 'Write your review first'
+                            }
+                        })}
+                        class="textarea max-w-sm textarea-bordered h-24" placeholder="Add your Review"></textarea>
+                    <label class="label">
+                        {errors.review?.type === 'required' && <span className="label-text-alt text-red-500">{errors?.review?.message}</span>}
+                    </label>
+                </div>
+                <input className='btn btn-primary w-full max-w-sm text-white' type="submit" value='Add Review' />
+            </form>
         </div>
     );
 };
