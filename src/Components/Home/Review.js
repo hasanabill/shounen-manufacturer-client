1c06594b02bbedd4b7393604553278168fc5cc56
hasanabill <hasanabilna@gmail.com>
2022-05-26 10:04:34 +0600

    loading reviews in homepage

---
+++ src/Components/Home/Review.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
+import ReviewCard from './ReviewCard';
+
+const Review = () => {
+
+    const { data: reviews, isLoading } = useQuery('review', () => fetch('http://localhost:5000/review').then(res => res.json()))
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+    return (
+        <div className='m-8'>
+            <h1 className="text-3xl text-center font-bold">Reviews</h1>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 justify-items-center'>
+                {
+                    reviews.map(review => <ReviewCard
+                        key={review._id}
+                        rev={review}
+                    ></ReviewCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Review;
---
+++ src/Components/Home/ReviewCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import avatar from '../../assets/images/avater.png'
+
+const ReviewCard = ({ review: rev }) => {
+    const { username, img, review } = rev;
+    return (
+        <div class="card w-96 bg-base-100 shadow-xl">
+            <div class="avatar card-body items-center p-0 pt-10">
+                <div class="w-16 rounded-full">
+                    <img src={img || avatar} alt='' />
+                </div>
+            </div>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">{username}</h2>
+                <p>{review}</p>
+            </div>
+        </div>
+    );
+};
+
+export default ReviewCard;
